0d2cfec5ff369d9660d440c4994dc5ce9039ae71
Dominic <dominicleonardwhite@gmail.com>
2026-09-20 17:43:08 +1000

    updated for slightly more complex website that uses env

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+import os
+import socket
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+APP_MESSAGE = os.environ.get("APP_MESSAGE", "Fallback message.<br><br>Override with:<br> <code>-e APP_MESSAGE=...</code>")
+PORT = int(os.environ.get("PORT", 5000))
+
+
 @app.route("/")
 def hello_world():
-    return "Hello world, this is running in a docker container"
+    return render_template(
+        "index.html",
+        message = APP_MESSAGE,
+        hostname=socket.gethostname(),
+        port=PORT
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
